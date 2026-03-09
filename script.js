d5be17ece3b0941a60f0320230869d5c702307bd
pritidalvi19 <pritidalvi18@gmail.com>
2026-03-09 17:05:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function validateForm(){
+
+let name=document.getElementById("name").value;
+let email=document.getElementById("email").value;
+
+if(name=="" || email==""){
+alert("Please fill all required fields");
+return false;
+}
+
+alert("Your enquiry has been submitted!");
+
+return true;
+
+}
